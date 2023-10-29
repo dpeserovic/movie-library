d672cc05ee1cb6bb95f8204947ff4f9c7b17414d
Dario Pešerović <d.peserovic@gmail.com>
2023-10-29 20:18:01 +0100

    implement discover movie api call

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,31 @@
+import discoverService from './api/services/discoverService';
+import MovieModel from './model/MovieModel';
+import EmptyResponseModel from './model/EmptyResponseModel';
+import PageableTable from './components/PageableTable';
+
+const MOVIE_TABLE_COLUMNS = [
+  { field: 'originalTitle', headerName: 'Title' },
+  { field: 'genre', headerName: 'Genre' },
+  { field: 'releaseDate', headerName: 'Release date' },
+];
+
 const App = () => {
-  return <div></div>
+  const apiCall = async (filter) => {
+    try {
+      const { page } = filter;
+      const response = await discoverService.getMovies(`page=${page}`);
+      response.results = response.results.map(i => new MovieModel(i));
+      if (response.total_pages > 500) response.total_pages = 500;
+      return response;
+    } catch (e) {
+      console.error(e);
+      return new EmptyResponseModel();
+    }
+  }
+
+  return (
+    <PageableTable columns={MOVIE_TABLE_COLUMNS} apiCall={apiCall} />
+  );
 }
 
 export default App;
