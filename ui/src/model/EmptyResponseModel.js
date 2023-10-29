8cff6ce093dd6d9d67fd8dda2896d7939236ebd0
Dario Pešerović <d.peserovic@gmail.com>
2023-10-29 20:17:31 +0100

    implement empty response model

---
+++ ui/src/model/EmptyResponseModel.js
@@ -0,0 +1,10 @@
+class EmptyResponseModel {
+    constructor() {
+        this.page = 0;
+        this.results = [];
+        this.total_pages = 0;
+        this.total_results = 0;
+    }
+}
+
+export default EmptyResponseModel;
