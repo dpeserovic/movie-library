352ec27bad1bcfc73aa90685b8fdaf7695d8d3af
Dario Pešerović <d.peserovic@gmail.com>
2023-10-29 22:52:45 +0100

    implement actions and style table

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,17 @@
+import { GridActionsCellItem } from '@mui/x-data-grid';
+import InfoIcon from '@mui/icons-material/Info';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 import discoverService from './api/services/discoverService';
 import MovieModel from './model/MovieModel';
 import EmptyResponseModel from './model/EmptyResponseModel';
 import PageableTable from './components/PageableTable';
 
 const MOVIE_TABLE_COLUMNS = [
-  { field: 'originalTitle', headerName: 'Title' },
-  { field: 'genre', headerName: 'Genre' },
-  { field: 'releaseDate', headerName: 'Release date' },
+  { field: 'originalTitle', headerName: 'Title', flex: 1, disableColumnMenu: true },
+  { field: 'genre', headerName: 'Genre', flex: 1, disableColumnMenu: true, sortable: false },
+  { field: 'releaseDate', headerName: 'Release date', flex: 1, disableColumnMenu: true },
+  { field: 'actions', type: 'actions', getActions: (props) => [<GridActionsCellItem icon={<InfoIcon />} onClick={(e) => console.log(props)} label='Info' />, <GridActionsCellItem icon={<EditIcon />} onClick={() => console.log(props)} label='Edit' />, <GridActionsCellItem icon={<DeleteIcon />} onClick={() => console.log(props)} label='Delete' />] }
 ];
 
 const App = () => {
--- ui/src/components/PageableTable.jsx
+++ ui/src/components/PageableTable.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { DataGrid, GridPagination } from '@mui/x-data-grid';
 import MuiPagination from '@mui/material/Pagination';
+import { tablePaginationClasses } from '@mui/material';
 
 const PageableTable = ({ columns, apiCall }) => {
     const [isLoading, setIsLoading] = useState(false);
@@ -24,39 +25,48 @@ const PageableTable = ({ columns, apiCall }) => {
     return (
         <>
             {tableData != null ?
-                <DataGrid
-                    columns={columns}
-                    initialState={{
-                        pagination: {
-                            paginationModel: { page: 0, pageSize: 20 },
-                        },
-                    }}
-                    loading={isLoading}
-                    onPaginationModelChange={(a, b, c) => console.log({ a, b, c })}
-                    pageSizeOptions={[20]}
-                    rows={tableData.results}
-                    slots={{ pagination: () => <CustomPagination tableData={tableData} setPage={setPage} /> }}
-                />
+                <div style={{ height: 800, width: '100%' }}>
+                    <DataGrid
+                        columns={columns}
+                        initialState={{
+                            pagination: {
+                                paginationModel: { page: 0, pageSize: 20 },
+                            },
+                        }}
+                        loading={isLoading}
+                        pageSizeOptions={[20]}
+                        rows={tableData.results}
+                        rowSelection={false}
+                        slots={{ pagination: () => <CustomPagination tableData={tableData} setPage={setPage} /> }}
+                    />
+                </div>
                 : null}
         </>
     );
 }
 
 const CustomPagination = ({ tableData: { page, total_pages }, setPage }) => {
-    return <GridPagination ActionsComponent={() => <Pager totalPages={total_pages} page={page} setPage={setPage} />} labelDisplayedRows={() => <Records currentPage={page} totalPages={total_pages} />} />
+    return (
+        <GridPagination
+            ActionsComponent={() => <Pager totalPages={total_pages} page={page} setPage={setPage} />}
+            labelDisplayedRows={() => <Records currentPage={page} totalPages={total_pages} />}
+            sx={{
+                [`& .${tablePaginationClasses.spacer}`]: {
+                    display: 'none',
+                },
+            }}
+        />
+    )
 }
 
 const Pager = ({ totalPages, page, setPage }) => {
     const onPageChange = (event, pageNumber) => setPage(pageNumber);
-
-    return (
-        <MuiPagination page={page} count={totalPages} showFirstButton={true} showLastButton={true} onChange={onPageChange} />
-    )
+    return <MuiPagination page={page} count={totalPages} showFirstButton={true} showLastButton={true} onChange={onPageChange} />;
 
 }
 
 const Records = ({ currentPage, totalPages }) => {
-    return <span className="MuiTablePagination-displayedRows css-levciy-MuiTablePagination-displayedRows">{currentPage} / {totalPages} Pages</span>
+    return <span>{currentPage} / {totalPages} Pages</span>;
 }
 
 export default PageableTable;
