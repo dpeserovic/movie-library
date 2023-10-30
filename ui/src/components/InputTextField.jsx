4f19362303b8d5bb5a972c464929ff712a8a3114
Dario Pešerović <d.peserovic@gmail.com>
2023-10-30 08:54:18 +0100

    implement input text component

---
+++ ui/src/components/InputTextField.jsx
@@ -0,0 +1,7 @@
+import TextField from '@mui/material/TextField';
+
+const InputTextField = (props) => {
+    return <TextField id="outlined-basic" variant="outlined" {...props} />
+}
+
+export default InputTextField;
